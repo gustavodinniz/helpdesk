0c3c9f02771bacea1b0aa10913a8a4b87efab2a7
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-02-08 17:15:10 -0300

    implementing findAll for technician

--- helpdesk-api/src/main/java/br/com/gustavodiniz/helpdesk/controllers/TechnicianController.java
+++ helpdesk-api/src/main/java/br/com/gustavodiniz/helpdesk/controllers/TechnicianController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/technical")
 public class TechnicianController {
@@ -21,5 +24,12 @@ public class TechnicianController {
     public ResponseEntity<TechnicianDTO> findById(@PathVariable Integer id) {
         Technician technician = service.findById(id);
         return ResponseEntity.ok().body(new TechnicianDTO(technician));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TechnicianDTO>> findAll() {
+        List<Technician> list = service.findAll();
+        List<TechnicianDTO> listDTO = list.stream().map(TechnicianDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
--- helpdesk-api/src/main/java/br/com/gustavodiniz/helpdesk/services/TechnicianService.java
+++ helpdesk-api/src/main/java/br/com/gustavodiniz/helpdesk/services/TechnicianService.java
@@ -6,6 +6,7 @@ import br.com.gustavodiniz.helpdesk.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class TechnicianService {
     public Technician findById(Integer id) {
         Optional<Technician> technician = repository.findById(id);
         return technician.orElseThrow(() -> new EntityNotFoundException("Technician with id " + id + " not found"));
+    }
+
+    public List<Technician> findAll() {
+         return repository.findAll();
     }
 }
